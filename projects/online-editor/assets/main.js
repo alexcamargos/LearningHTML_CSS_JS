e27ecce40977680cc4055b7632d7dcc47f707494
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2021-05-31 12:37:32 -0300

    Simples Online Web Editor.

--- projects/online-editor/assets/main.js
+++ projects/online-editor/assets/main.js
@@ -1,5 +1,21 @@
+/*
+# -------------------------------------------------------------------------------------------------
+#
+# Name: main.js
+# Version: 1.0
+#
+# Summary: Simples Online Web Editor
+#
+# Author: Alexsander Lopes Camargos
+# Author-email: alcamargos@vivaldi.net
+#
+# License: MIT
+#
+# -------------------------------------------------------------------------------------------------
+*/
+
+
 function refresh() {
     let textContent = document.getElementById('editor').value;
-    console.log(textContent)
     document.getElementById('viewer').srcdoc = textContent;
 }
