f55f83f02bff3f3f577ebaa8707dbdb95c0e0c32
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2021-06-18 20:09:53 -0300

    Yet Another Random Password Generator - YARPG
    yarpg.js file refactoring.

--- projects/random-password-generator/assets/js/yarpg.js
+++ projects/random-password-generator/assets/js/yarpg.js
@@ -3,16 +3,14 @@ const yarpg = document.getElementById('yarpg');
 const lengthValue = document.getElementById('length_value');
 const passwordLength = document.getElementById('password-length');
 
-
 const digits = '0123456789';
-const punctuation = '!\"#$ %&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+const punctuation = '!"#$ %&\'()*+,-./:;<=>?@[\\]^_`{|}~';
 
 const ascii_lowercase = 'abcdefghijklmnopqrstuvwxyz';
 const ascii_uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 const ascii_letters = ascii_lowercase + ascii_uppercase;
 const printable = digits + ascii_letters + punctuation;
-
 
 function changePasswordLengthValue() {
     updatePasswordLengthValue(lengthValue, passwordLength);
@@ -21,7 +19,6 @@ function changePasswordLengthValue() {
 function updatePasswordLengthValue(element, value) {
     element.innerHTML = value.value;
 }
-
 
 function getPassword(passwordLength, characters) {
     let password = '';
@@ -34,45 +31,46 @@ function getPassword(passwordLength, characters) {
     return password;
 }
 
-
 function passwordGenerator() {
     let hasUppercase = document.getElementById('ascii_uppercase');
     let hasLowercase = document.getElementById('ascii_lowercase');
     let hasDigits = document.getElementById('digits');
     let hasPunctuation = document.getElementById('punctuation');
 
-
     if (!hasPunctuation.checked) {
         yarpg.value = getPassword(passwordLength.value, ascii_letters + digits);
-
     } else if (!hasDigits.checked) {
-        yarpg.value = getPassword(passwordLength.value, ascii_letters + punctuation);
-
+        yarpg.value = getPassword(
+            passwordLength.value,
+            ascii_letters + punctuation
+        );
     } else if (!hasLowercase.checked) {
-        yarpg.value = getPassword(passwordLength.value, ascii_uppercase + digits + punctuation);
-
+        yarpg.value = getPassword(
+            passwordLength.value,
+            ascii_uppercase + digits + punctuation
+        );
     } else if (!hasUppercase.checked) {
-        yarpg.value = getPassword(passwordLength.value, ascii_lowercase + digits + punctuation);
-
-    } else if (!hasPunctuation.checked, !hasDigits.checked) {
+        yarpg.value = getPassword(
+            passwordLength.value,
+            ascii_lowercase + digits + punctuation
+        );
+    } else if ((!hasPunctuation.checked, !hasDigits.checked)) {
         yarpg.value = getPassword(passwordLength.value, ascii_letters);
-
-    } else if (!hasUppercase.checked, !hasLowercase.checked) {
+    } else if ((!hasUppercase.checked, !hasLowercase.checked)) {
         yarpg.value = getPassword(passwordLength.value, digits + punctuation);
-
-    } else if (!hasUppercase.checked, !hasPunctuation.checked) {
-        yarpg.value = getPassword(passwordLength.value, ascii_lowercase + digits);
-
+    } else if ((!hasUppercase.checked, !hasPunctuation.checked)) {
+        yarpg.value = getPassword(
+            passwordLength.value,
+            ascii_lowercase + digits
+        );
     } else {
         yarpg.value = getPassword(passwordLength.value, printable);
     }
 }
 
-
 function copyPassword() {
-
     let generateButton = document.querySelector('.btn-generate');
-    let showNewPassword = document.getElementById('new-password')
+    let showNewPassword = document.getElementById('new-password');
 
     // Select the text field.
     yarpg.select();
@@ -80,14 +78,13 @@ function copyPassword() {
     yarpg.setSelectionRange(0, 99999);
 
     // Copy the text inside the text field.
-    document.execCommand("copy");
+    document.execCommand('copy');
 
     showNewPassword.innerHTML = yarpg.value;
 
     alertBox.classList.toggle('alert-box__hidden');
 
     setTimeout(function () {
-        alertBox.classList.toggle('alert-box__hidden')
+        alertBox.classList.toggle('alert-box__hidden');
     }, 900);
-
 }
