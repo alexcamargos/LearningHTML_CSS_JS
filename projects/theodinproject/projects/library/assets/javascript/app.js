e7f2c88370d2bf2633af7ce316ca401413953400
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2022-10-27 12:30:51 -0300

    feat: skeleton of the website

---
+++ projects/theodinproject/projects/library/assets/javascript/app.js
@@ -0,0 +1,12 @@
+// -------------------------------------------------------------------------------------------------
+// Name: app.js
+// Version: 0.0.1
+//
+// Summary: alexcamargos.github.io
+//          My Personal Portfolio.
+//
+// Author: Alexsander Lopes Camargos
+// Author-email: alcamargos@vivaldi.net
+//
+// License: MIT
+// -------------------------------------------------------------------------------------------------
