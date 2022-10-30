ac6125ed006f46d4f90cace2c8b4d80474252c8b
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2022-10-30 17:29:15 -0300

    feat: complete system automation with javascript

--- projects/theodinproject/projects/library/assets/javascript/app.js
+++ projects/theodinproject/projects/library/assets/javascript/app.js
@@ -11,15 +11,301 @@
 // License: MIT
 // -------------------------------------------------------------------------------------------------
 
+// Identify the book to be edited.
+var bookID = 0;
 
-var dialog = document.getElementById("add_book_dialog");
+class Book {
+    // Book has a title, author, release year, total pages, pages read and a read status.
+    // The read status is a boolean value that can be changed by the user.
+    constructor(title, author, release, pages, pagesRead = 0, read = false) {
+        this.id = bookID;
+        this.title = title;
+        this.author = author;
+        this.release = release;
+        this.pages = pages;
+        this.pagesRead = pagesRead;
+        this.read = read;
+        bookID++;
+    }
+}
+
+class Library {
+    // Library has a list of books.
+    // The library has a method to add a book to the library.
+    // The library has a method to remove a book from the library.
+    // The library has a method to check if a book is in the library.
+    // The library has a method to get a book from the library.
+    // The library has a method to get all books from the library.
+    constructor() {
+        // Create a new library.
+        this.books = [];
+    }
+
+    addBook(newBook) {
+        // Add a book to the library.
+        this.books.push(newBook);
+    }
+
+    removeBook(title) {
+        // Remove a book from the library.
+        this.books = this.books.filter((book) => book.title !== title);
+    }
+
+    updateBook(updateBook) {
+        // Update a book in the library.
+        this.books = this.books.map((book) =>
+            book.title === updateBook.title ? updateBook : book
+        );
+    }
+
+    isInLibrary(book) {
+        // Check if a book is in the library.
+        return this.books.some((book) => book.title === book.title);
+    }
+
+    getBook(title) {
+        // Get a book from the library.
+        return this.books.find((book) => book.title === title);
+    }
+
+    getBooks() {
+        // Get all books from the library.
+        return this.books;
+    }
+
+    length() {
+        // Get the number of books in the library.
+        return this.books.length;
+    }
+
+    getTotalPages() {
+        // return the sum of all pages in each book in the library.
+        return this.books.reduce((total, book) => total + book.pages, 0);
+    }
+
+    getTotalPagesRead() {
+        // Get the total number of pages read in the library.
+        return this.books.reduce((total, book) => total + book.pagesRead, 0);
+    }
+
+    getTotalReadBooks() {
+        // Get the total number of read books in the library.
+        return this.books.filter((book) => book.read === true).length;
+    }
+}
+
+// Create a new library.
+var myLibrary = new Library();
+
+// Create a sample library.
+function populateLibrary() {
+    // A default data whit Game os Thrones books.
+    const DEFAULT_DATA = [
+        {
+            title: 'A Game of Thrones',
+            author: 'George R. R. Martin',
+            release: 1996,
+            pages: 694,
+            pagesRead: 694,
+            read: true,
+        },
+        {
+            title: 'A Clash of Kings',
+            author: 'George R. R. Martin',
+            release: 1998,
+            pages: 768,
+            pagesRead: 768,
+            read: true,
+        },
+        {
+            title: 'A Storm of Swords',
+            author: 'George R. R. Martin',
+            release: 2000,
+            pages: 973,
+            pagesRead: 973,
+            read: true,
+        },
+        {
+            title: 'A Feast for Crows',
+            author: 'George R. R. Martin',
+            release: 2005,
+            pages: 753,
+            pagesRead: 42,
+            read: false,
+        },
+        {
+            title: 'A Dance with Dragons',
+            author: 'George R. R. Martin',
+            release: 2011,
+            pages: 1016,
+            pagesRead: 0,
+            read: false,
+        },
+    ];
+
+    for (let i = 0; i < DEFAULT_DATA.length; i++) {
+        let book = new Book(
+            DEFAULT_DATA[i].title,
+            DEFAULT_DATA[i].author,
+            DEFAULT_DATA[i].release,
+            DEFAULT_DATA[i].pages,
+            DEFAULT_DATA[i].pagesRead,
+            DEFAULT_DATA[i].read
+        );
+
+        myLibrary.addBook(book);
+    }
+}
+
+// Show/Hide the dialog to add/edit a new book.
+var dialog = document.getElementById('add_book_dialog');
+var overlay = document.getElementById('overlay');
 
 function showDialog() {
+    // Show the dialog.
     dialog.show();
-    dialog.classList.add("dialog-scale");
+    dialog.classList.add('dialog-scale');
+    overlay.classList.add('active');
 }
 
 function closeDialog() {
+    // Hide the dialog.
     dialog.close();
-    dialog.classList.remove("dialog-scale");
+    dialog.classList.remove('dialog-scale');
+    overlay.classList.remove('active');
 }
+
+// Add new book to the library.
+function render() {
+    let bookList = document.getElementById('books-library');
+    bookList.innerHTML = '';
+
+    let totalBooks = document.getElementById('total-books');
+    totalBooks.innerHTML = `${myLibrary.length()}`;
+
+    let totalPages = document.getElementById('total-pages');
+    totalPages.innerHTML = `${myLibrary.getTotalPages()}`;
+
+    let totalPagesRead = document.getElementById('total-pages-read');
+    totalPagesRead.innerHTML = `${myLibrary.getTotalPagesRead()}`;
+
+    let totalReadBooks = document.getElementById('read-books');
+    totalReadBooks.innerHTML = `${myLibrary.getTotalReadBooks()}`;
+
+    let addBook = document.createElement('div');
+    addBook.classList.add('add-book');
+
+    // When the user clicks on the add book button, the dialog is shown.
+    addBook.addEventListener('click', showDialog);
+    addBook.innerHTML = `
+            <picture>
+                <img
+                    src="assets/img/icons/add-book.svg"
+                    alt="Add Book"
+                />
+            </picture>
+        `;
+
+    // For each book in the library, create a div with the book's information.
+    myLibrary.getBooks().forEach((book) => {
+        let bookCard = document.createElement('div');
+        bookCard.classList.add('book-card');
+        bookCard.innerHTML = `
+                <div class="book-card-header">
+                    <h5>Title: ${book.title}</h5>
+                    <span>Author: ${book.author}</span>
+                </div>
+                <div class="book-card-body">
+                    <span>Release Year: ${book.release}</span>
+                    <span>Total Pages: ${book.pages}</span>
+                    <span>Pages Read: ${book.pagesRead}</span>
+                    <span>Read: ${book.read ? 'Read' : 'Not Read'}</span>
+                </div>
+
+                <div class="book-card-footer">
+                    <button id="btn-edit" class="btn_edit">Edit</button>
+                    <button id="btn-delete" class="btn_delete">Delete</button>
+                </div>
+            `;
+        bookList.appendChild(bookCard);
+        bookList.appendChild(addBook);
+    });
+}
+
+function addBookToLibrary() {
+    // Add a new book to the library.
+    let title = document.getElementById('book-title').value;
+    let author = document.getElementById('book-author').value;
+    let release = parseInt(document.getElementById('book-release-year').value);
+    let pages = parseInt(document.getElementById('book-total-pages').value);
+    let pagesRead = parseInt(document.getElementById('book-pages-read').value);
+    let read = document.getElementById('book-read').checked;
+
+    // Create a new book.
+    let book = new Book(title, author, release, pages, pagesRead, read);
+    myLibrary.addBook(book);
+
+    closeDialog();
+    render();
+}
+
+// On page load, populate the library with the default data.
+window.addEventListener('load', () => {
+    populateLibrary();
+    render();
+});
+
+// Update the book in the library.
+function updateBookInLibrary(book) {
+    myLibrary.updateBook(book);
+}
+
+// Delete/Edit a book from the library.
+document.addEventListener('click', (element) => {
+    // If the user clicks on the delete button, the book is removed from the library.
+    if (element.target && element.target.id == 'btn-delete') {
+        let title =
+            element.target.parentNode.parentNode.children[0].children[0]
+                .innerHTML;
+        title = title.split(':')[1].trim();
+        myLibrary.removeBook(title);
+
+        render();
+    }
+
+    // If the user clicks on the edit button, the dialog is shown with the book's information.
+    if (element.target && element.target.id == 'btn-edit') {
+        // Get the book for edit.
+        let title =
+            element.target.parentNode.parentNode.children[0].children[0]
+                .innerHTML;
+        title = title.split(':')[1].trim();
+        // Get the book from the library.
+        let book = myLibrary.getBook(title);
+
+        // Populate the dialog with the book's information.
+        document.getElementById('book-title').value = book.title;
+        document.getElementById('book-author').value = book.author;
+        document.getElementById('book-release-year').value = book.release;
+        document.getElementById('book-total-pages').value = book.pages;
+        document.getElementById('book-pages-read').value = book.pagesRead;
+        document.getElementById('book-read').checked = book.read;
+        showDialog();
+
+        // When the user clicks on the save button,
+        // the old book is removed from the library and a new one is added.
+        myLibrary.removeBook(book.title);
+
+        // Create a new book with the new information.
+        title = document.getElementById('book-title').value;
+        author = document.getElementById('book-author').value;
+        release = parseInt(document.getElementById('book-release-year').value);
+        pages = parseInt(document.getElementById('book-total-pages').value);
+        pagesRead = parseInt(document.getElementById('book-pages-read').value);
+        read = document.getElementById('book-read').checked;
+        newBook = new Book(title, author, release, pages, pagesRead, read);
+        updateBookInLibrary(newBook);
+
+        render();
+    }
+});
