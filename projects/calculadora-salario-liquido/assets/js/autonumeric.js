e94d55a63dae643b940936b4254e29d67a627370
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2022-11-03 20:44:32 -0300

    feat: main javascript application

---
+++ projects/calculadora-salario-liquido/assets/js/autonumeric.js
@@ -0,0 +1,25 @@
+// -------------------------------------------------------------------------------------------------
+// Name: autonumeric.js
+// Version: 0.0.1
+//
+// Summary: Calculadora de Salário Líquido - Salário bruto x Salário Líquido
+//
+// Author: Alexsander Lopes Camargos
+// Author-email: alcamargos@vivaldi.net
+//
+// License: MIT
+// -------------------------------------------------------------------------------------------------
+
+// Default options.
+const autoNumericOptionsReal = {
+    currencySymbol: 'R$ ',
+    decimalCharacter: ',',
+    digitGroupSeparator: '.',
+    minimumValue: '0',
+};
+
+// Initialize AutoNumeric.
+new AutoNumeric('#salario-bruto', autoNumericOptionsReal);
+new AutoNumeric('#valor-pensao', autoNumericOptionsReal);
+new AutoNumeric('#previdencia-privada', autoNumericOptionsReal);
+new AutoNumeric('#outros-valores', autoNumericOptionsReal);
